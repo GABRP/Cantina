d9a82276f2308f223a721f132852b929cd9c064f
Gabriel da Costa Rodrigues <112523344+GABRP@users.noreply.github.com>
2023-04-05 15:33:32 -0300

    feat: concluido2!

--- src/br/newtonpaiva/Massa.java
+++ src/br/newtonpaiva/Massa.java
@@ -1,6 +1,7 @@
 package br.newtonpaiva;
 
 import javax.swing.*;
+import java.awt.*;
 
 class Massa {
     private String tipo;
@@ -20,8 +21,35 @@ class Massa {
     }
 
     public static String escolherTipo() {
-        Object[] opcoes = {"Penne", "Espaguete", "Ravioli"};
-        int opcao = JOptionPane.showOptionDialog(null, "Escolha o tipo de massa:", "Tipo de Massa", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
-        return (String) opcoes[opcao];
+        final String[] opcaoSelecionada = {null};
+        final JDialog dialog = new JDialog();
+        dialog.setModal(true);
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.setLayout(new BorderLayout());
+        dialog.setTitle("Tipo de Massa");
+
+        JLabel label = new JLabel("Escolha o tipo de massa:");
+        label.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        dialog.add(label, BorderLayout.NORTH);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        String[] opcoes = {"Penne", "Espaguete", "Ravioli"};
+        for (String opcao : opcoes) {
+            JButton button = new JButton(opcao);
+            button.addActionListener(e -> {
+                opcaoSelecionada[0] = opcao;
+                dialog.dispose();
+            });
+            buttonPanel.add(button);
+        }
+
+        dialog.add(buttonPanel, BorderLayout.CENTER);
+        dialog.pack();
+        dialog.setLocationRelativeTo(null);
+        dialog.setVisible(true);
+
+        return opcaoSelecionada[0];
     }
 }
